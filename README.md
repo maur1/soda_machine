aaee21c01f1578f3e4d5627d3c8813399eac0c26
Maureen <maureen@live.no>
2022-08-29 07:40:30 +0200

    add readme, remove unused import

---
+++ README.md
@@ -0,0 +1,5 @@
+Soda Machine
+=======================
+
+Soda machine mock that takes user inputs and outputs according to selected output.
+
--- src/test/java/TestVendingMachine.java
+++ src/test/java/TestVendingMachine.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestInsert {
+public class TestVendingMachine {
 
 
     //System.out.println("Returning " + this.credit + " to customer");
